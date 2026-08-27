68809f830989b4e6b891961a4490568b7db68ab8
ManjunathAnde <manjunathande7@gmail.com>
2026-08-27 16:22:51 -0400

    Error handling block

--- sysargv.py
+++ sysargv.py
@@ -1,6 +1,13 @@
 import sys
 filename = sys.argv[1] #first arguement is always script name
 
-'''prints and opens the contents of the first filename we enter in terminal '''
-with open(filename, "r") as f:
-    print(f.read()) 
+'''prints and opens the contents of the first filename we enter in terminal with error handling '''
+try:
+    with open(filename, "r") as f:
+        print(f.read())
+
+except IndexError:
+    print("Please provide a filename")
+except FileNotFoundError:
+    print("There is no such file in the project folder")
+
