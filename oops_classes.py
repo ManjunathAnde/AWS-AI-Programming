d9ba58567849d09944d56fbca8bf899e9f271022
ManjunathAnde <manjunathande7@gmail.com>
2026-08-28 18:50:11 -0400

    Arthimetic operations methods

--- oops_classes.py
+++ oops_classes.py
@@ -10,3 +10,20 @@ class Student:
     def __init__(self, name, scores=None): #default arguement
         self.name = name                    # instance attribute
         self.scores = scores if scores else [] #if no scores, then empty list
+    def add_score(self, score):
+        """Add a single score."""
+        self.scores.append(score)
+
+    def average(self):
+        """Calculate average score."""
+        if not self.scores: #If no scores exist, return 0
+            return 0
+        return sum(self.scores) / len(self.scores)
+
+    def highest(self):
+        """Return the highest score."""
+        if not self.scores:
+            return 0
+        return max(self.scores)
+
+    
